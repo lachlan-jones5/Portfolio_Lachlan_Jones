7fef29583ce9680f64e78a70d86f02d01c75c6d7
lj428 <69785383+lj428@users.noreply.github.com>
2021-02-18 20:43:55 -0800

    Update LongestIncreasingSubsequence.cs

--- LongestIncreasingSubsequence/LongestIncreasingSubsequence(C#)/LongestIncreasingSubsequence.cs
+++ LongestIncreasingSubsequence/LongestIncreasingSubsequence(C#)/LongestIncreasingSubsequence.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp1
 {
     class LongestIncreasingSubsequence
     {
+		//This function is a binary search for the smallest value greater than or equal to the value given
         static int BSearch(List<int> array, int[] T, int left, int right, int val)
         {
 
@@ -24,36 +25,38 @@ namespace ConsoleApp1
             return right;
 
         }
+		//This function finds and prints the longest increasing subsequence given from user input
         static void LIS(List<int> arr)
         {
 
-            int N = arr.Capacity;
+            int N = arr.Capacity;				//Set N = the amount of numbers given
 
-            int[] tailIndicies = new int[N];
+            int[] tailIndicies = new int[N];	//Tail indicies array holds all possible end values
 
-            for (int i = 0; i < N; i++)
+            for (int i = 0; i < N; i++)			//Initalise to 0
                 tailIndicies[i] = 0;
 
-            int[] prevIndicies = new int[N];
+            int[] prevIndicies = new int[N];	//Prev indicies holds all parent values
 
             for (int i = 0; i < N; i++)
-                prevIndicies[i] = -1;
+                prevIndicies[i] = -1;			//Initialise to -1
 
-            int len = 1;
+            int len = 1;						//The longest subsqeuence - currently is 1 as each element is a subsequence of 1
 
+			//Iterate through the given elements
             for (int i = 1; i < N; i++)
             {
-
-                if (arr[i] <= arr[tailIndicies[0]])
+				
+				if (arr[i] <= arr[tailIndicies[0]])
                     tailIndicies[0] = i;
-                else if (arr[i] > arr[tailIndicies[len - 1]])
+                else if (arr[i] > arr[tailIndicies[len - 1]]) //If it is bigger than the previous element, then we can add it in
                 {
 
                     prevIndicies[i] = tailIndicies[len - 1];
                     tailIndicies[len++] = i;
 
                 }
-                else
+                else //If it is not big enough to add, we find a place for it to be added to
                 {
 
                     int x = BSearch(arr, tailIndicies, -1, len - 1, arr[i]);
@@ -64,15 +67,14 @@ namespace ConsoleApp1
 
             }
 
-
-            //Do the print part now
+			//Create a new array to hold the LIS
             int[] myArr = new int[N];
             int myArrIndex = 0;
 
             for (int i = tailIndicies[len - 1]; i >= 0; i = prevIndicies[i])
                 myArr[myArrIndex++] = arr[i];
 
-
+			//Print the LIS
             for (int i = myArrIndex - 1; i >= 0; i--)
                 Console.Write(myArr[i] + " ");
 
